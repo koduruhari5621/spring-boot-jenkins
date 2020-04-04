a6df1c378ddfcd9fdc5634b501dd9efb4f466d0d
Koduru, Hari Krishna <C-HariKrishna.Koduru@charter.com>
2020-04-04 15:49:18 +0530

    Added New End Point

---
+++ src/main/java/com/pcf/demo/model/DemoModel.java
@@ -0,0 +1,12 @@
+package com.pcf.demo.model;
+
+import lombok.Data;
+
+@Data
+public class DemoModel {
+	
+	private String name;
+	
+	private String section;
+
+}
--- src/main/java/com/pcf/demo/service/api/DemoController.java
+++ src/main/java/com/pcf/demo/service/api/DemoController.java
@@ -1,8 +1,12 @@
 package com.pcf.demo.service.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pcf.demo.model.DemoModel;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -11,6 +15,13 @@ public class DemoController {
 	@GetMapping("/test")
 	public String demoTest() {
 		return "Hello from Demo Application deployed in PCF";
+	}
+
+	@PostMapping("/post")
+	public DemoModel demoPost(@RequestBody DemoModel demoModel) {
+
+		return demoModel;
+
 	}
 
 }
